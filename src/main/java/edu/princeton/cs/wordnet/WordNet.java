dd775e10cc60966f265f811e095ba8f59494f9b7
Luis Galicia <luisgalicia8@hotmail.com>
2020-02-28 13:58:25 -0400

    added tests and functionality for null arguments handling

--- src/main/java/edu/princeton/cs/wordnet/WordNet.java
+++ src/main/java/edu/princeton/cs/wordnet/WordNet.java
@@ -8,8 +8,15 @@ import edu.princeton.cs.algs4.Bag;
 
 public class WordNet {
 
+    String nullArgMessage = "arguments can not be null";
+
     // constructor takes the name of two input files
     WordNet(String synsets, String hypernims) {
+
+        if (synsets == null || hypernims == null) {
+            throw new IllegalArgumentException(nullArgMessage);
+        }
+
         System.out.println(synsets + " " + hypernims);
     }
 
@@ -20,17 +27,32 @@ public class WordNet {
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
+
+        if (word == null) {
+            throw new IllegalArgumentException(nullArgMessage);
+        }
+
         return false;
     }
 
     // distance between nounA and nounB (see definitions.txt)
     public int distance(String nounA, String nounB) {
+
+        if (nounA == null || nounB == null) {
+            throw new IllegalArgumentException(nullArgMessage);
+        }
+
         return 0;
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA
     // and nounB in a shortest ancestral path (see definitions.txt)
     public String sap(String nounA, String nounB) {
+        
+        if (nounA == null || nounB == null) {
+            throw new IllegalArgumentException(nullArgMessage);
+        }
+
         return "stub";
     }
 
--- src/test/java/edu/princeton/cs/wordnet/WordnetTests.java
+++ src/test/java/edu/princeton/cs/wordnet/WordnetTests.java
@@ -4,74 +4,59 @@
  *  Description: Tests for ./src/WordnetTests.java
  **************************************************************************** */
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-
-import edu.princeton.cs.algs4.Digraph;
-import edu.princeton.cs.algs4.In;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 public class WordnetTests {
     WordNet wordnet0 = new WordNet("synsets100-subgraph.txt", "hypenyms100-subgraph");
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     @Test
-    public void shouldTestIllegalNullArguments() throws IllegalArgumentException {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Arguments must not be null");
+    void testIllegalNullArguments() {
 
-        WordNet test;
-        test = new WordNet(null, "hypernyms.txt");
-        test = new WordNet("synsets.txt", null);
-        test.isNoun(null);
-        test.distance(null, "Hola");
-        test.distance("Tedious", null);
-        test.sap(null, "Lel");
-        test.sap("ciao", null);
+        // should split up, but wanted to try the assertAll syntax 
+
+        assertAll("constructor",
+                () -> assertThrows(IllegalArgumentException.class, 
+                                   () -> new WordNet(null, "hypernyms.txt")),
+                () -> assertThrows(IllegalArgumentException.class, 
+                                   () -> new WordNet("synsets.txt", null)),
+                () -> assertThrows(IllegalArgumentException.class,
+                                   () -> new WordNet(null, null)));
+
+
+        WordNet testObj = new WordNet("synsets.txt", "hypernyms.txt");
+
+        assertAll("instance methods",
+                () -> assertThrows(IllegalArgumentException.class,
+                                   () -> testObj.isNoun(null)),
+
+                () -> assertThrows(IllegalArgumentException.class,
+                                   () -> testObj.distance(null, "Abel")),
+                () -> assertThrows(IllegalArgumentException.class,
+                                   () -> testObj.distance("Abel", null)),
+                () -> assertThrows(IllegalArgumentException.class,
+                                   () -> testObj.distance(null, null)),
+
+                () -> assertThrows(IllegalArgumentException.class,
+                                   () -> testObj.sap("Abel", null)),
+                () -> assertThrows(IllegalArgumentException.class,
+                                   () -> testObj.sap(null, "Abel")),
+                () -> assertThrows(IllegalArgumentException.class,
+                                   () -> testObj.sap(null, null)));
     }
 
     @Test
-    public void shouldTestIllegalNonRootedDAGForConstructor() throws IllegalArgumentException {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Hypernym relationships are not a DAG");
-
-        new WordNet("not relevant", "cyclicDigraph.txt");
+    void testIllegalConstructorArgumentNonRootedDAG() {
     }
 
     @Test
-    public void shouldTestNonWordNetNounAForDistanceMethod() throws IllegalArgumentException {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("yibiyabo is not a WordNet noun");
-
-        wordnet0.distance("yibiyabo", "chondrin");
+    void testIllegalDistanceMethodArgumentNotAWordNetNoun() {
     }
 
     @Test
-    public void shouldTestNonWordNetNounBForDistanceMethod() throws IllegalArgumentException {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("quickmaffs is not a WordNet noun");
+    void testIllegalSAPMethodArgumentNotAWordNetNoun() {
 
-        wordnet0.distance("elastin", "quickmaffs");
-    }
-
-    @Test
-    public void shouldTestNonWordNetNounAForSAPMethod() throws IllegalArgumentException {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("fufufufufu is not a WordNet noun");
-
-        wordnet0.sap("fufufufufu", "filaggrin");
-    }
-
-    @Test
-    public void shouldTestNonWordNetNounBForSAPMethod() throws IllegalArgumentException {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("illuminatus is not a WordNet noun");
-
-        wordnet0.sap("gelatin", "illuminatus");
     }
 }
